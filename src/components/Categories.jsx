5504ab31d99ecaea9514472db69e55a5fd58c226
SkyKeyUA <veremiienkoov96@gmail.com>
2022-12-29 20:22:30 +0200

    create new components Card/Categories/Header/Sort

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+	return (
+		<div class="categories">
+		<ul>
+		  <li class="active">All</li>
+		  <li>Meat</li>
+		  <li>Vegetarian</li>
+		  <li>Grilled</li>
+		  <li>Pepperoni</li>
+		  <li>Closed</li>
+		</ul>
+	 </div>
+	);
+}
+
+export default Categories;
