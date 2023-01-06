533528d15bdddcf8cad83418dce8a00218738714
SkyKeyUA <veremiienkoov96@gmail.com>
2023-01-06 21:15:32 +0200

    create search filter in component Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from "react";
+/** @format */
 
-import Home from "./pages/Home";
-import Cart from "./pages/Cart"
-import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
 
 import './scss/app.scss';
 function App() {
-	return (
-		<div className="wrapper">
-			<Header/>
+  const [searchValue, setSearchValue] = React.useState('');
+  console.log(searchValue);
+  return (
+    <div className="wrapper">
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
-			<Routes>
-				<Route path="/" element={<Home/>}/>
-				<Route path="/cart" element={<Cart/>}/>
-				<Route path="*" element={<NotFound/>}/>
-			</Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home searchValue={searchValue} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </div>
-	);
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,69 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
 
 import Categories from '../components/Categories';
-import Sort from "../components/Sort";
-import Card from "../components/Card";
-import Skeleton from "../components/Card/Skeleton";
+import Sort from '../components/Sort';
+import Card from '../components/Card';
+import Skeleton from '../components/Card/Skeleton';
+import Pagination from '../components/Pagination';
 
-import axios from "axios";
+import axios from 'axios';
 
-function Home() {
-	const [items, setItems] = React.useState([]);
-	const [isLoading, setIsLoading] = React.useState(true);
-	const [categoryId, setCategoryId] = React.useState(0);
-	const [sortType, setSortType] = React.useState({
-		name: 'popularity',
-		sortProperty: 'rating',
-	});
-	React.useEffect(() => {
-		setIsLoading(true);
-		// fetch('https://63b2b99f5e490925c51fc1ea.mockapi.io/items')
-		// .then((res) => res.json())
-		// .then((json) => {setItems(json); setIsLoading(false);})
-		const axiosItems = async () => {
-			const category = categoryId > 0 ? `category=${categoryId}` : '';
-			const sortBy = sortType.sortProperty.replace('-', '');
-			const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
-			const data = await axios(`https://63b2b99f5e490925c51fc1ea.mockapi.io/items?${
-				category
-				}&sortBy=${sortBy}&order=${order}`)
-				.then((res) => { setItems(res.data) });
-			setIsLoading(false);
-		};
-		axiosItems();
-		window.scrollTo(0, 0);
-	}, [categoryId, sortType]);
-	return (
-		<div className="container">
-			<div className="content__top">
-				<Categories
-					categoryId={categoryId}
-					onClickCategory={(i) => setCategoryId(i)}
-				/>
-				<Sort
-					sortType={sortType}
-					onClickSort={(i) => setSortType(i)}
-				/>
-			</div>
-			<h2 className="content__title">All Pizzas</h2>
-			<div className="content__items">
-				{isLoading ? [...Array(12)].map((_, index) =>
-					<Skeleton key={index} />)
-					: items.map((obj) => (
-						<Card
-							key={obj.id}
-							// title= {obj.title}
-							// price= {obj.price}
-							// imageUrl= {obj.imageUrl}
-							// sizes= {obj.sizes}
-							// types= {obj.types}
-							{...obj}
-						/>
-					))}
-			</div>
-		</div>
-	)
+function Home({ searchValue }) {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'popularity',
+    sortProperty: 'rating',
+  });
+  React.useEffect(() => {
+    setIsLoading(true);
+    // fetch('https://63b2b99f5e490925c51fc1ea.mockapi.io/items')
+    // .then((res) => res.json())
+    // .then((json) => {setItems(json); setIsLoading(false);})
+    const axiosItems = async () => {
+      const category = categoryId > 0 ? `category=${categoryId}` : '';
+      const search = searchValue ? `&search=${searchValue}` : '';
+      const sortBy = sortType.sortProperty.replace('-', '');
+      const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+      const data = await axios(
+        `https://63b2b99f5e490925c51fc1ea.mockapi.io/items?page=1&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      ).then((res) => {
+        setItems(res.data);
+      });
+      setIsLoading(false);
+    };
+    axiosItems();
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType, searchValue]);
+  //   const filterItems = items.filter((item) =>
+  //     item.title.toLowerCase().includes(searchValue.toLowerCase()),
+  //   );
+  const skeletons = [...new Array(12)].map((_, index) => <Skeleton key={index} />);
+  const pizzas = items.map((obj) => (
+    <Card
+      key={obj.id}
+      // title= {obj.title}
+      // price= {obj.price}
+      // imageUrl= {obj.imageUrl}
+      // sizes= {obj.sizes}
+      // types= {obj.types}
+      {...obj}
+    />
+  ));
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories categoryId={categoryId} onClickCategory={(i) => setCategoryId(i)} />
+        <Sort sortType={sortType} onClickSort={(i) => setSortType(i)} />
+      </div>
+      <h2 className="content__title">All Pizzas</h2>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination />
+    </div>
+  );
 }
 
 export default Home;
