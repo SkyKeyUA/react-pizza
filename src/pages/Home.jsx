cb2eaabc6ee4deb0b4a45eaae858548fa522c556
SkyKeyUA <veremiienkoov96@gmail.com>
2023-01-10 13:10:36 +0200

    was created setCategoryId/setSort in filterSlice and used in page Home and component Sort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,20 +15,22 @@ import { setCategoryId } from '../redux/slices/filterSlice';
 
 function Home() {
   const dispatch = useDispatch();
-  const categoryId = useSelector((state) => state.filterSlice.categoryId);
+  const { categoryId, sort } = useSelector((state) => state.filterSlice);
+  //const sortType = sort.sortProperty;
+  //   const categoryId = useSelector((state) => state.filterSlice.categoryId);
+  //   const sortType = useSelector((state) => state.filterSlice.sort.sortProperty);
   const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   // const [categoryId, setCategoryId] = React.useState(0);
   const [currentPage, setCurrentPage] = React.useState(1);
-  const [sortType, setSortType] = React.useState({
-    name: 'popularity',
-    sortProperty: 'rating',
-  });
+  //   const [sortType, setSortType] = React.useState({
+  //     name: 'popularity',
+  //     sortProperty: 'rating',
+  //   });
   const onClickCategory = (id) => {
     dispatch(setCategoryId(id));
   };
-  console.log(categoryId);
   React.useEffect(() => {
     setIsLoading(true);
     // fetch('https://63b2b99f5e490925c51fc1ea.mockapi.io/items')
@@ -37,8 +39,8 @@ function Home() {
     const axiosItems = async () => {
       const category = categoryId > 0 ? `category=${categoryId}` : '';
       const search = searchValue ? `&search=${searchValue}` : '';
-      const sortBy = sortType.sortProperty.replace('-', '');
-      const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+      const sortBy = sort.sortProperty.replace('-', '');
+      const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
       const data = await axios(
         `https://63b2b99f5e490925c51fc1ea.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
       ).then((res) => {
@@ -48,7 +50,7 @@ function Home() {
     };
     axiosItems();
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue, currentPage]);
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
   //   const filterItems = items.filter((item) =>
   //     item.title.toLowerCase().includes(searchValue.toLowerCase()),
   //   );
@@ -68,7 +70,7 @@ function Home() {
     <div className="container">
       <div className="content__top">
         <Categories categoryId={categoryId} onClickCategory={onClickCategory} />
-        <Sort sortType={sortType} onClickSort={(i) => setSortType(i)} />
+        <Sort />
       </div>
       <h2 className="content__title">All Pizzas</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
